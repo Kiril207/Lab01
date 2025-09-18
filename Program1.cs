a9a8796ec83c4d7f7f06ed5bb5674298d54d9723
Kiril207 <kirya.sherstyuk.05@mail.ru>
2025-09-18 22:24:33 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program1
+{
+    static void Main()
+    {
+        Console.WriteLine($"sbyte: Min= {sbyte.MinValue}, Max= {sbyte.MaxValue}");
+        Console.WriteLine($"byte: Min = {byte.MinValue}, Max= {byte.MaxValue}");
+        Console.WriteLine($"short: Min={short.MinValue}, Max={short.MaxValue}");
+        Console.WriteLine($"ushort: Min={ushort.MinValue}, Max={ushort.MaxValue}");
+        Console.WriteLine($"int: Min={int.MinValue}, Max={int.MaxValue}");
+        Console.WriteLine($"uint: Min={uint.MinValue}, Max={uint.MaxValue}");
+        Console.WriteLine($"long: Min={long.MinValue}, Max={long.MaxValue}");
+        Console.WriteLine($"ulong: Min={ulong.MinValue}, Max={ulong.MaxValue}");
+        Console.WriteLine();
+        Console.WriteLine("Числа с плавающей точкой");
+        Console.WriteLine($"float: Min={float.MinValue}, Max={float.MaxValue}");
+        Console.WriteLine($"double: Min={double.MinValue}, Max={double.MaxValue}");
+        Console.WriteLine($"decimal: Min={decimal.MinValue}, Max={decimal.MaxValue}");
+        Console.WriteLine();
+        Console.WriteLine("Символьныый тип");
+        Console.WriteLine($"char: Min={char.MinValue}, Max={char.MaxValue}");
+        Console.WriteLine();
+        Console.WriteLine("Логический тип");
+        Console.WriteLine($"bool: Values={false},{true}");
+    }
+}
